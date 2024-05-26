342ad3531556c6c47103d555567f1a13e337e316
thu <thu.vo25@student.passerellesnumeriques.org>
2024-05-26 12:37:01 +0700

    Finish User

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Navbar from './components/layout/Navbar';
+import { useEffect, useState } from 'react';
+import axios from "axios"
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar/>
+        <div className='container'>
+          <h1>Github Users Data</h1>
+        </div>
+        <Search />
     </div>
   );
 }
